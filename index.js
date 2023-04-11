cfefffe70ee91e109cf03456a4872263ef9f8f76
Drew <drwomble27@gmail.com>
2023-04-11 11:39:19 -0600

    complete

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(someValue){
+  if (someValue < 400){
+    someValue = "This one is on me!";
+  }
+  else if ((someValue > 400) && (someValue < 2000)){
+    someValue = "That will be twenty bucks.";
+  }
+  else if ((someValue > 2000) && (someValue < 2500)){
+    someValue = "I will gladly take your thirty bucks.";
+  }
+  else if (someValue > 2500){
+    someValue = "No can do.";
+  }
+  return someValue;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    tip = "Thank you so much.";
+  }
+  else if (tip === "not as generous" ){
+    tip = "Thank you.";
+  }
+  else if (tip !== "generous"){
+    tip = "Bye.";
+  }
+  return tip;
 }
